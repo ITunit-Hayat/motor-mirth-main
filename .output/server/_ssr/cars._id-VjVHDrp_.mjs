38f876f97d877b3a5f724189c4e6f6a31fd54cee
ITunit-Hayat <bansalahilyes@gmail.com>
2026-08-22 16:54:00 +0100

    delet

--- .output/server/_ssr/cars._id-VjVHDrp_.mjs
+++
@@ -1,6 +0,0 @@
-import { _ as createFileRoute, g as lazyRouteComponent } from "../_libs/@tanstack/react-router+[...].mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/cars._id-VjVHDrp_.js
-var $$splitComponentImporter = () => import("./cars._id-DmpWcJBl.mjs");
-var Route = createFileRoute("/cars/$id")({ component: lazyRouteComponent($$splitComponentImporter, "component") });
-//#endregion
-export { Route as t };
--- .output/server/_ssr/start-BKuld8x0.mjs
+++
@@ -1,6 +0,0 @@
-import { n as createCsrfMiddleware, t as createStart } from "./ssr.mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/start-BKuld8x0.js
-var csrfMiddleware = createCsrfMiddleware({ filter: (ctx) => ctx.handlerType === "serverFn" });
-var startInstance = createStart(() => ({ requestMiddleware: [csrfMiddleware] }));
-//#endregion
-export { startInstance };
